dd82103da634b420565f539d9bdc3cf7258b2b23
Abhishek Kr Jha <Abhishekkjha068@outlook.com>
2024-12-04 18:52:26 +0530

    Added model mapper

--- src/main/java/com/bloggingApp/com/bloggApp_api/BloggAppApiApplication.java
+++ src/main/java/com/bloggingApp/com/bloggApp_api/BloggAppApiApplication.java
@@ -1,13 +1,22 @@
 package com.bloggingApp.com.bloggApp_api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BloggAppApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloggAppApiApplication.class, args);
+
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
+
+
