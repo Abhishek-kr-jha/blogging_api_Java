33a905880d4b8a3fcfcac4eb8da19ae4ac4fe491
Abhishek Kr Jha <Abhishekkjha068@outlook.com>
2024-12-11 20:17:54 +0530

    Added validator

--- src/main/java/com/bloggingApp/com/bloggApp_api/payloads/UserDto.java
+++ src/main/java/com/bloggingApp/com/bloggApp_api/payloads/UserDto.java
@@ -2,7 +2,9 @@ package com.bloggingApp.com.bloggApp_api.payloads;
 
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,12 +15,14 @@ import lombok.Setter;
 public class UserDto {
     private int id;
 
-    @NotNull
+    @NotEmpty
+    @Size(min = 4, message = "Username must be min of 4 characters")
     private String name;
-    @Email(message = "Email is required")
+    @Email(message = "Email address is not valid")
     private String email;
-    @NotNull(message = "Password is required")
+    @NotEmpty
+    @Size(min= 3, max = 10, message = "Password must be min of 3 chars and maximum of 10 chars" )
     private String password;
-    @NotNull
+    @NotEmpty
     private String about;
 }
